795af3cb175bdf731669b1b7260a82119bd68f96
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2018-06-23 15:50:35 -0700

    Login Page - First Draft

---
+++ XamarinApp/XamarinApp/XamarinApp/Models/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApp.Models
+{
+    public class Constants
+    {
+        public static bool IsDev = true;
+
+    }
+}
---
+++ XamarinApp/XamarinApp/XamarinApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinApp.Models;
+
+namespace XamarinApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+        }
+
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+
+            if (user.CheckInformation())
+                //title message and cancel
+                DisplayAlert("Login","Login Successful","Ok");
+            else
+                DisplayAlert("Login", "Login Not Correct, empty username or password", "Ok");
+        }
+	}
+}
